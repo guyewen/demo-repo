beb2ca9257f7e338af570c8f5376991613e5f36e
Yewen Gu <Yewen.Gu@nhh.no>
2021-02-22 16:50:47 +0100

    Added qDebug message Hello World

--- Hello-World/main.cpp
+++ Hello-World/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QtDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    qDebug() << "Hello Wrold";
     return a.exec();
 }
